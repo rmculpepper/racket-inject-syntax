163a9bb42939bea539f5ad6dfcb3f3a87b9c3908
Ryan Culpepper <ryanc@racket-lang.org>
2020-02-21 14:34:28 +0100

    add code

---
+++ inject-syntax-lib/info.rkt
@@ -0,0 +1,12 @@
+#lang info
+
+;; pkg info
+
+(define version "1.0")
+(define collection "inject-syntax")
+(define deps '(["base" #:version "6.3"]))
+(define pkg-authors '(ryanc))
+
+;; collection info
+
+(define name "inject-syntax")
